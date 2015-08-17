7d53ee72fc7cf2f99e571c46842b535383a65400
sandeep <sandeepsinghsisodiya98@gmail.com>
2015-08-17 19:41:38 +0530

    Added heroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://scheduled-mailer.herokuapp.com/' }
 config.action_mailer.delivery_method = :smtp
 # change to true to allow email to be sent during development
 config.action_mailer.perform_deliveries = true
