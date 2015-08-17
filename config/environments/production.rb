6e781a9fee3913b6dcc3a4d89640a45f5c61a927
sandeep <sandeepsinghsisodiya98@gmail.com>
2015-08-17 20:29:18 +0530

    removed scalar settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.after_initialize do
-    Delayed::Job.scaler = :heroku_cedar
-  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
